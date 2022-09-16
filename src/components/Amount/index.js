b7f8a12b1cdc2337aaf60f9d68e0c47c5dd62e5d
unknown <fredericreact@gmail.com>
2022-09-16 13:28:07 +0800

    static components done

--- src/components/Amount/index.js
+++ src/components/Amount/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import './styles.scss'
 
 const Amount = () => (
-  <div>
-    Amount
-  </div>
+  <div className="amount">
+      <p className="amount-value">1.09</p>
+      <p className="amount-currency">USD</p>
+      
+    </div>
 );
 
 export default Amount;
